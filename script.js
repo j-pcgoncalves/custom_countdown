6c810c133a945fe2e76df00117c18acce37dae1a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-08-15 22:05:37 +0100

    add setInterval function

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const timeElements = document.querySelectorAll('span');
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
+let countdownActive;
 
 const second = 1000;
 const minute = second * 60;
@@ -22,26 +23,28 @@ dateEl.setAttribute('min', today);
 
 // Populate Countdown / Complete UI
 const updateDOM = () => {
-    const now = new Date().getTime();
-    const distance = countdownValue - now;
+    countdownActive = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = countdownValue - now;
 
-    const days = Math.floor(distance / day);
-    const hours = Math.floor((distance % day) / hour);
-    const minutes = Math.floor((distance % hour) / minute);
-    const seconds = Math.floor((distance % minute) / second);
+        const days = Math.floor(distance / day);
+        const hours = Math.floor((distance % day) / hour);
+        const minutes = Math.floor((distance % hour) / minute);
+        const seconds = Math.floor((distance % minute) / second);
 
-    // Populate Countdown
-    countdownElTitle.textContent = `${countdownTitle}`;
-    timeElements[0].textContent = `${days}`;
-    timeElements[1].textContent = `${hours}`;
-    timeElements[2].textContent = `${minutes}`;
-    timeElements[3].textContent = `${seconds}`;
-    
-    // Hide Input
-    inputContainer.hidden = true;
+        // Populate Countdown
+        countdownElTitle.textContent = `${countdownTitle}`;
+        timeElements[0].textContent = `${days}`;
+        timeElements[1].textContent = `${hours}`;
+        timeElements[2].textContent = `${minutes}`;
+        timeElements[3].textContent = `${seconds}`;
+        
+        // Hide Input
+        inputContainer.hidden = true;
 
-    // Show Countdown
-    countdownEl.hidden = false;
+        // Show Countdown
+        countdownEl.hidden = false;
+    }, second);
 }
 
 // Take Values from Form Input
